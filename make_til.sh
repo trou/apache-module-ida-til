195a355269208d4452c0d6891cf74ec38e6448df
Raphaël Rigo <raphael.rigo@airbus.com>
2021-03-25 14:12:23 +0100

    make_til.sh

--- make_til.sh
+++ make_til.sh
@@ -4,7 +4,8 @@ export NLSPATH=$HOME/bin/ida76
 TILIB=/home/raph/tools/reverse/IDA/v7.6/tools/tilib76/
 
 ALL_HDR=apache2.2-all.h
-gcc -Ihttpd-2.2.34/os/unix/ -Ihttpd-2.2.34/include/ -Ihttpd-2.2.34/srclib/apr/include/ -Ihttpd-2.2.34/srclib/apr-util/include/ -E apr_all.h | grep -v "^# " > $ALL_HDR
+HTTPD=httpd-2.2.34
+gcc -I$HTTPD/os/unix/ -I$HTTPD/include/ -I$HTTPD/srclib/apr/include/ -I$HTTPD/srclib/apr-util/include/ -I$HTTPD/modules/filters/ -E apache_all.h | grep -v "^# " > $ALL_HDR
 
 $TILIB/linux/tilib64 @gcc64.cfg \
         -c \
@@ -12,15 +13,18 @@ $TILIB/linux/tilib64 @gcc64.cfg \
         "-h$ALL_HDR" \
         -D__extension__= \
         "-D__builtin_va_list=void *" \
-        apache22-apr17-debian64.til | grep -v "Unrecognized attribute"
-exit 0
+        -t"Apache 2.2 - Linux 64bit" \
+        apache22-debian64.til | grep -v "Unrecognized attribute"
 
-gcc -Iapache2-2.4.46/os/unix/ -Iapache2-2.4.46/include/ -Iapr-1.7.0/include/ -Iapr-util-1.6.1/include/ -E apr_all.h | grep -v "^# " > apr-1.7-all.h
+ALL_HDR=apache2.4-all.h
+gcc -DAPACHE24 -Ihttpd-2.4.46/os/unix/ -Ihttpd-2.4.46/include/ -Ihttpd-2.4.46/srclib/apr/include/ -Ihttpd-2.4.46/srclib/apr-util/include/ -E apache_all.h | grep -v "^# " > $ALL_HDR
  
-$TILIB/linux/tilib @gcc64.cfg \
+$TILIB/linux/tilib64 @gcc64.cfg \
         -c \
-        -b$NLSPATH/til/pc/gnulnx_x64.til \
-        -hapr-1.7-all.h \
+        "-b$NLSPATH/til/pc/gnulnx_x64.til" \
+        "-h$ALL_HDR" \
         -D__extension__= \
         "-D__builtin_va_list=void *" \
-        apr-17-debian64.til | grep -v "Unrecognized attribute"
+        apache24-apr17-debian64.til | grep -v "Unrecognized attribute"
+
+cp apache24-apr17-debian64.til apache22-debian64.til "$NLSPATH/til/"
